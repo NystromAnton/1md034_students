3ec26a3c36adcaa5c7a761cf91205b9bd4f83d8b
Anton Nyström <anton1nystrom@gmail.com>
2019-02-06 17:09:19 +0100

    Some javascript

---
+++ public/js/vue_script.js
@@ -0,0 +1,21 @@
+function MenuItem(name, kcal, gluten, lactose, img_url) {
+  this.name = name;
+  this.kcal = kcal;
+  this.gluten = gluten;
+  this.lactose = lactose;
+  this.img_url = img_url;
+}
+
+var burg1 = new MenuItem('The Bacon Burger', 950, 'true', 'false', 'img/baconburger.jpg');
+var burg2 = new MenuItem('The Chicken Burger', 730, 'true', 'true', 'img/chickenburger.jpg');
+var burg3 = new MenuItem('The Halloumi Burger', 820, 'true', 'true', 'img/halloumiburger.jpg');
+var burg4 = new MenuItem('The Nacho Burger', 890, 'true', 'false', 'img/nachoburger.jpg');
+var burg5 = new MenuItem('The Non-Burger', 10, 'false', 'false', 'img/nonburger.jpg');
+var menuarr = [burg1, burg2, burg3, burg4, burg5];
+
+//new Vue({
+  //el: '#burgervue',
+  //data: {
+    //menu: menuarr
+  //},
+//})
